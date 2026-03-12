8a97a0ea81fc43649fff5c3fe93fa7d3be974107
Alefioarv <fioale200@gmail.com>
2026-03-12 10:33:30 +0100

    fix

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
         args = parse_args()
     else:
         args = None
+    
     selection_function(args)
--- src/selection.py
+++ src/selection.py
@@ -9,7 +9,8 @@ from .print_promt import print_promt, flush
 def selection_function(args: list):
     while True:
         print("\033[3J\033[H", end="")
-        print_promt(args=args)
+        args = print_promt(args=args)
+        """ breakpoint() """
         flush()
         random_42 = True
         cols, rows = args[0], args[1]
@@ -24,9 +25,10 @@ def selection_function(args: list):
             m.random_draw_42(cols, rows)
         else:
             m.draw_42(cols, rows)
-        m.backtracking(m.grid[0][0], True, True)
+        m.backtracking(m.grid[0][0], True, args[4])
         m.bfs(True)
         m.print_maze()
         m.print_hexa_maze("hexa.txt")
+        input("\nPress ENTER to continue...")
         print('\033[3J\033[H')
 
