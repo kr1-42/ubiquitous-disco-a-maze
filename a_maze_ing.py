4ba906f653ed16cac7bfbdd5b34fd481255dd3fa
Christian Lombardi <chrilomb@c1r4p12.42firenze.it>
2026-04-08 14:24:54 +0200

    lal

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     if (len(argv) != 1):
         args = parse_args()
     else:
-        args = cast(MazeArgs, None)
+        print("\033[31mNo arguments provided. usage ... \n python3 a_maze_ing.py <maze_file>\033[0m")
+        exit(1)
     try:
         print_promt(args=args)
     except (EOFError, KeyboardInterrupt) as e:
